bb854f88672c80041dac72fdacb2e325c58bcfd8
Butta Leelavathi <buttaleela1@gmail.com>
2025-04-14 20:57:08 +0530

    Stir

---
+++ spoon.java
@@ -0,0 +1,19 @@
+public class Spoon 
+{
+String material;
+int height;
+String Texture;
+String Colour;
+String Shape;
+}
+//Functions
+spoonWash() {
+}
+Spoonfeed () {
+}
+SpoonStir () {
+}
+SpoonMeasure () {
+}
+SpoonServe() {
+}
