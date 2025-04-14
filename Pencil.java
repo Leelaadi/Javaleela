7828ab24239884b5ddea3b713319c4164ba7ef1b
Butta Leelavathi <buttaleela1@gmail.com>
2025-04-14 18:03:29 +0530

    Erase

---
+++ Pencil.java
@@ -0,0 +1,13 @@
+public class Pencil {
+public static void main (String[] args) {
+System.out.println(" Write"); //Print this line
+System.out.println("Pencil can erase");
+System.out.println("Write" + "Erase");
+System.out.println(" \"Write\" ");
+System.out.println( " Pencil \n can \n \n \n erase");
+System.out.println("Hi \t\n Can we buy pencil in that shop \t\n Stationaries");
+System.out.println(" We can break the Pencil into \\ two pieces");
+System.out.println ("Break \"two pieces\" " );
+
+}
+}
