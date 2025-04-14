0e767125af443dd95172593dae4576acaa0dae73
Butta Leelavathi <buttaleela1@gmail.com>
2025-04-14 19:10:29 +0530

    Switch On

---
+++ Laptop.java
@@ -0,0 +1,19 @@
+public class Laptop 
+{
+String Companyname;
+int Weight;
+int Width;
+String Colour;
+int Price;
+}
+//Functions
+Mouse Scrolling() {
+}
+Cameraon/Cameraoff() {
+}
+Chargingon/Chargingoff() {
+}
+Mikeon/mikeoff() {
+}
+Laptopon/Laptopclose() {
+}
